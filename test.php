6d1afe48de1e515742cebe21bdb423ab5f7dbf8a
taedium8vitae <gabguinn@bluewin.ch>
2022-01-09 21:19:01 +0100

    update test.php

--- test.php
+++ test.php
@@ -1,24 +1,21 @@
 <?php
 try
 {
-	// On se connecte à MySQL
-	$mysqlClient = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', 'root');
+	$mysqlClient = new PDO('mysql:host=localhost;dbname=test;charset=utf8',
+	'root',
+	'root');
 }
 catch(Exception $e)
 {
-	// En cas d'erreur, on affiche un message et on arrête tout
         die('Erreur : '.$e->getMessage());
 }
 
-// Si tout va bien, on peut continuer
 
-// On récupère tout le contenu de la table recipes
 $sqlQuery = 'SELECT * FROM test_table';
 $recipesStatement = $mysqlClient->prepare($sqlQuery);
 $recipesStatement->execute();
 $recipes = $recipesStatement->fetchAll();
 
-// On affiche chaque recette une à une
 foreach ($recipes as $recipe) {
 ?>
     <p><?php echo $recipe['name']; ?></p>
