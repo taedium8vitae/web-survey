f8d808d16231218546ba23a80ce7274296b8cfd9
taedium8vitae <gabguinn@bluewin.ch>
2022-02-08 17:14:34 +0100

    Making css fonts work

--- c5D6z9n6WC/index.php
+++ c5D6z9n6WC/index.php
@@ -34,22 +34,8 @@ This website's purpose is actually to record the user's behavior on websites. Do
 	<html>
 		<head>
 			<meta charset="utf-8"/>
-			<link rel="stylesheet" href="styles/main.css"/>
 			<title>Web Survey</title>
-			<style>
-				@font-face{
-					font-family: "miki";
-					src: url("fonts/regular.otf") format("opentype");
-				}
-				@font-face{
-					font-family: "miki_bold";
-					src: url("fonts/bold.otf") format("opentype");
-				}
-				@font-face{
-					font-family: miki_extra_bold;
-					src: url("fonts/extrabold.otf");
-				}
-			</style>
+			<link rel="stylesheet" href="/styles/main.css"/>
 		</head>
 		<body>
 			<!-- HEADER -->
